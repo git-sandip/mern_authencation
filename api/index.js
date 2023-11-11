cb07268f47c274a7d9d46f9b721e9904e005439f
Sandip Sapkota <sandipsapkota9844@gmail.com>
2023-11-11 21:46:26 +0545

    module based js implemented

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL)
@@ -15,3 +16,5 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`🚀Server started on port: ${PORT}`);
 });
+
+app.use("/api/user", userRoutes);

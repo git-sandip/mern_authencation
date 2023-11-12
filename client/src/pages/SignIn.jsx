bb825fd72c5e4687e13f0a8a837a2b2475b53b71
Sandip Sapkota <sandipsapkota9844@gmail.com>
2023-11-12 11:40:13 +0545

    added redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInFaliure,
+  signInSuccess,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 const SignIn = () => {
   const [formData, setformData] = useState({});
-  const [error, seterror] = useState(false);
-  const [loading, setloading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
+
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setformData({ ...formData, [e.target.id]: e.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setloading(true);
+      dispatch(signInStart());
       const res = await fetch("http://localhost:3000/api/auth/login", {
         method: "POST",
         headers: {
@@ -21,16 +28,14 @@ const SignIn = () => {
       });
       const data = await res.json();
 
-      if (data.success == false) {
-        seterror(true);
+      if (data.success === false) {
+        dispatch(signInFaliure(data));
         return;
       }
-      seterror(false);
-      setloading(false);
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      setloading(true);
-      seterror(true);
+      dispatch(signInFaliure(error));
     }
   };
   return (
@@ -52,17 +57,18 @@ const SignIn = () => {
           onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70">
-          {loading ? "Loading..." : "Signup"}
+          {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
       <div className="flex gap-3 mt-5">
         <p>Don&apos;t Have an account?</p>
         <Link to="/register">
-          <span className="text-blue-500">Sign Up</span>
+          <span className="text-blue-500">Sign Un</span>
         </Link>
       </div>
-      {console.log(error)}
-      <p className="text-red-700 mt-5">{error && "Something Went Wrong"}</p>
+      <p className="text-red-700 mt-5">
+        {error ? error.error || "Something Went Wrong" : ""}
+      </p>
     </div>
   );
 };

1cea3db0b44fc7ce6792e831d9c189b15d4a2255
Sandip Sapkota <sandipsapkota9844@gmail.com>
2023-11-12 07:40:54 +0545

    implemenented hashed password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js";
-
+import bcryptjs from "bcryptjs";
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({
     username,
     email,
-    password,
+    password: hashedPassword,
   });
   try {
     await newUser.save();

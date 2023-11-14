ad8ad12222c116c546368934de72086505dfb4d1
Sandip Sapkota <sandipsapkota9844@gmail.com>
2023-11-14 09:23:24 +0545

    issue fixed

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
-app.use(cors(corsOptions));
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`🚀Server started on port: ${PORT}`);
